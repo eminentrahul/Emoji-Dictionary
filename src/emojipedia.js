2eafe68ac14913dd8937ca2db12aa037a5bbb3ab
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-09 18:01:06 +0530

    Code Optimization

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,5 +19,56 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😷",
+    name: "Face With Medical Mask",
+    meaning:
+      "The mask is for protection. Either to protect yourself or others from infection. Someone is in the hospital, has to go to the doctor or has caught a disease. Wearing the masks is widely spread in Asia."
+  },
+  {
+    id: 5,
+    emoji: "🤫",
+    name: "Exhorting Face",
+    meaning:
+      "Shhh! is meant to silence others. Or someone is confiding a secret that should not be retold. Nicely telling your counterpart to shut up."
+  },
+  {
+    id: 6,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:
+      "The thinker's pose: Thoughtful face with the characteristic hand on the chin. Puts a statement, a person's intelligence, or an idea into question. Is mulling over something or thinking about a brilliant idea."
+  },
+  {
+    id: 7,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+  },
+  {
+    id: 8,
+    emoji: "😝",
+    name: " Face With Stuck-Out Tongue & Tightly Closed Eyes",
+    meaning:
+      "Making fun of someone, annoying or jokingly offending someone. In provocative news, crude or black humor as well as with glee."
+  },
+  {
+    id: 9,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning:
+      "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude."
+  },
+  {
+    id: 10,
+    emoji: "🥰",
+    name: "Smiling face with smiling eyes and three hearts",
+    meaning:
+      "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart."
   }
 ];
+
+export default emojipedia;
